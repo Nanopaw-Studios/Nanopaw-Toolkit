122eed2609423878f0fc0b0a37f895a54db8a4fd
Laym <65513977+LaymGlitched@users.noreply.github.com>
2025-11-29 22:33:41 +0000

    Nanomusic

--- Runtime/NanodogsToolkit/NanoMusic/NanoMusicAsset.cs
+++ Runtime/NanodogsToolkit/NanoMusic/NanoMusicAsset.cs
@@ -2,12 +2,21 @@
 
 namespace Nanodogs.API.NanoMusic
 {
+    [CreateAssetMenu(fileName = "New Nano Music Asset", menuName = "Nanodogs/Nano Music Asset", order = 1)]
     public class NanoMusicAsset : ScriptableObject
     {
+        public NanoSong songData;
         public AudioClip musicClip;
         public bool loop = true;
         public float volume = 1.0f;
         public float pitch = 1.0f;
         public bool playOnAwake = false;
+    }
+
+    [System.Serializable]
+    public class NanoSong
+    {
+        public string songName;
+        public string artistName;
     }
 }
